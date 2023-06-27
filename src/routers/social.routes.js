5eed27f050f4255cfbd8321a9d481e58edd97732
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-06-27 00:42:53 -0500

    End of social media functionality

--- src/routers/social.routes.js
+++ src/routers/social.routes.js
@@ -5,12 +5,20 @@ const { isAuthenticated } = require('../helpers/validate-auth')
 
 const { renderSocialForm,
         renderAllSocial,
-        createNewProfileSocial
+        createNewProfileSocial,
+        deleteSocial,
+        renderEditSocialForm,
+        updateSocial
 } = require('../controllers/social.controller.js')
 
 router.get('/social', isAuthenticated, renderAllSocial)
 
 router.get('/social/add', isAuthenticated, renderSocialForm)
 router.post('/social/add', isAuthenticated, createNewProfileSocial)
+
+router.get('/social/edit/:id', isAuthenticated, renderEditSocialForm)
+router.put('/social/edit/:id', isAuthenticated, updateSocial)
+
+router.delete('/social/delete/:id', isAuthenticated, deleteSocial)
 
 module.exports = router
